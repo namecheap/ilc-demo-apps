e5e63d5b64e2ac577fce383c984700bbffb8ee34
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-10-16 19:30:55 +0300

    'ilc-sdk/dist/app' -> 'ilc-sdk/app'

--- apps/navbar/src/server.js
+++ apps/navbar/src/server.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { ServerLocation } from '@reach/router'
 import App from './root.component';
-import IlcAppSdk from 'ilc-sdk/dist/app';
+import IlcAppSdk from 'ilc-sdk/app';
 
 export default function (ilcReqData, url, links = []) {
     const clientSdk = new IlcAppSdk(ilcReqData);
