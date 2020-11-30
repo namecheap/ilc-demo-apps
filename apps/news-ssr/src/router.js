138164cacfbbe491614095c5a0f95ee067f3cd3a
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-11-30 15:47:05 +0200

    fix: fixed 404 page handling for News app

--- apps/news-ssr/src/router.js
+++ apps/news-ssr/src/router.js
@@ -17,6 +17,7 @@ export const createRouter = () => {
 				children: [
 					{ path: 'news/', component: Home }, // () => import('../components/Home.vue')
 					{ path: 'news/article/:source', component: Articles}, // () => import('../components/Articles.vue')
+					{ path: '*', name: '404', component: NotFound },
 				]
 			},
 			{ path: '*', name: '404', component: NotFound }
