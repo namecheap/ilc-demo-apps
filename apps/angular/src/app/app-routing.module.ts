d9e3fb19e9ef5d5ef92644472158a1c16506e800
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-02-03 14:20:02 +0200

    feat: basic CSR integration with ILC

--- apps/angular/src/app/app-routing.module.ts
+++ apps/angular/src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import {EmptyRouteComponent} from './empty-route/empty-route.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: '**', component: EmptyRouteComponent }
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes, {
     initialNavigation: 'enabled'
 }) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/angular' }],
 })
 export class AppRoutingModule {}
--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
+import {EmptyRouteComponent} from "./empty-route/empty-route.component";
 
 @NgModule({
   imports: [
@@ -35,7 +36,8 @@ import { MessagesComponent } from './messages/messages.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    EmptyRouteComponent,
   ],
   bootstrap: [ AppComponent ]
 })
