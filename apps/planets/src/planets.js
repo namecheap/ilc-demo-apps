db8bdf21dd8a65320b80d91adb3d8b4c5d96a683
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-03-10 14:15:19 +0200

    chore: unnecessary comment removal

--- apps/planets/src/planets.js
+++ apps/planets/src/planets.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-//import singleSpaVue from 'single-spa-vue' FIXME: this should be updated with forked library in NPM
 import singleSpaVue from 'ilc-adapter-vue'
 import configuredRouter from './router.js'
 
